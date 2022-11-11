8e3ceca7a40678867b7c79d65d47ef9719361532
mehedi-hasan-azad <mehediazad4@gmail.com>
2022-11-11 21:50:15 +0600

    Space added

--- app/src/main/java/com/example/architectureexample/MainActivity.java
+++ app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int ADD_NOTE_REQUEST = 1;
     public static final int EDIT_NOTE_REQUEST = 2;
 
+
     private NoteViewModel noteViewModel;
     private RecyclerView recyclerView;
     private NoteAdapter noteAdapter;
